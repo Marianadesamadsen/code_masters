7dc1caf4be563c91d35229afa24a26e54722b0cb
Marianadesamadsen <s201205@student.dtu.dk>
2026-04-28 20:59:21 +0200

    move graphs

---
+++ GNN_training/one_wave/startup_tests/test_startup.py
@@ -0,0 +1,21 @@
+from neural_lam.train_model import main
+
+args = [
+"--config_path" ,"GNN_training\one_wave\yaml_files\config_wave_28_ts_600_g4_sigmamin_15.yaml" ,
+"--graph", "GNN_training\graphs\gsub4_msub4_nn1",
+"--loss", "mse",
+"--seed", "42",
+"--eval","test",
+"--num_workers" ,"0",
+"--epochs" , "100",
+"--processor_layers", "1" ,
+"--logger_run_name", "test_startup_bs_32",
+"--batch_size", "32" ,
+"--logger-project" ,"startup_waves" ,
+"--precompute_in_memory", "True",
+"--save_eval_to_zarr_path", "GNN_training\one_wave\startup_tests\results"
+]
+
+main(args)
+
+
